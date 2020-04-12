847a516472d4607397c1d02389d132f1f61b49c1
DimiDimit <dmtrdmtrov@gmail.com>
2020-04-12 12:29:21 +0300

    Fixed control panel using old admin check

--- src/app/admin.guard.ts
+++ src/app/admin.guard.ts
@@ -1,14 +1,13 @@
 import {Injectable} from '@angular/core';
 import {ActivatedRouteSnapshot, CanActivateChild, Router, RouterStateSnapshot, UrlTree} from '@angular/router';
 import {AngularFireAuth} from '@angular/fire/auth';
-import {AngularFirestore} from '@angular/fire/firestore';
 import {take} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AdminGuard implements CanActivateChild {
-  constructor(private router: Router, private afAuth: AngularFireAuth, private afs: AngularFirestore) {
+  constructor(private router: Router, private afAuth: AngularFireAuth) {
   }
 
   async canActivateChild(next: ActivatedRouteSnapshot, state: RouterStateSnapshot): Promise<boolean | UrlTree> {
@@ -16,6 +15,6 @@ export class AdminGuard implements CanActivateChild {
     if (user == null) {
       return this.router.parseUrl('/signin');
     }
-    return (await this.afs.firestore.doc('other/admins').get()).data()?.admins?.includes(user.uid);
+    return (await user.getIdTokenResult())?.claims?.admin;
   }
 }
